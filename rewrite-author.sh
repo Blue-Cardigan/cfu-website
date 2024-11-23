a698268e7528afaa8527ccbef3f395db39a07585
Red-Cardigan <jethro.reeve@gmail.com>
2024-11-23 17:08:06 +0000

    Added workflow to replace commit author phil with Red-Cardigan

--- rewrite-author.sh
+++ rewrite-author.sh
@@ -2,5 +2,5 @@
 git filter-repo --email-callback '
     return email if email != b"phil.a.lefevre@gmail.com" else b"jethro.reeve@gmail.com"
 ' --name-callback '
-    return name if email != b"phil.a.lefevre@gmail.com" else b"Jethro Reeve"
+    return name if email != b"phil.a.lefevre@gmail.com" else b"Red-Cardigan"
 '
