984b6394bf76146c11f26025a6a7c02aeb2b5fc5
Red-Cardigan <jethro.reeve@gmail.com>
2024-11-23 16:56:36 +0000

    Add rewrite-author.sh script

---
+++ rewrite-author.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git filter-repo --email-callback '
+    return email if email != b"phil.a.lefevre@gmail.com" else b"jethro.reeve@gmail.com"
+' --name-callback '
+    return name if email != b"phil.a.lefevre@gmail.com" else b"Jethro Reeve"
+'
